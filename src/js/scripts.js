878fcfaea9393f2f4c44160ffacecbad14d76538
Zekai Wu <zekaiwu@bu.edu>
2024-04-09 12:51:13 -0400

    Function normally

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,31 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+console.log("Hello!");
+
+const myName = "Zekai";
+const myAge = "23";
+console.log(myAge);
+
+// Practical JavaScript
+// Show and high an element on the page
+
+// Find an element to hide
+const toggledParagraph = document.querySelector(".show");
+console.log(toggledParagraph);
+
+// Write a function that toggles the show/hide classes
+
+function toggleParagraph() {
+	toggledParagraph.classList.toggle("hide");
+	toggledParagraph.classList.toggle("show");
+	console.log("My toggle function");
+	console.log(toggledParagraph);
+}
+
+//Find the button and make something happen when we click it
+const toggleButton = document.querySelector("#toggle-control");
+console.log(toggleButton);
+
+toggleButton.addEventListener("click", toggleParagraph)
